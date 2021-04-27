50fd49e762ce396597b18baf53dd5886e0264661
Bethsedea Joy Delos Angeles <bethsedeajoy.delosangeles@tractebel.engie.com>
2021-04-27 16:15:11 +0800

    add partial unittest for fx widget

--- fxWidgetApp/src/app/modules/fxwidget/fxwidget.component.spec.ts
+++ fxWidgetApp/src/app/modules/fxwidget/fxwidget.component.spec.ts
@@ -34,10 +34,45 @@ describe('FxwidgetComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FxwidgetComponent);
     component = fixture.componentInstance;
+    fxServiceSpy.getRate = () =>  2;
+    fxServiceSpy.getBuyRate = () =>  5;
+    fxServiceSpy.getSellRate = () =>  4;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle isBuy', () => {
+    expect(component.isBuy.value).toBeTruthy();
+    component.toggle(false);
+    expect(component.isBuy.value).toBeFalsy();
+  });
+
+  it('should update values', () => {
+    component.updateValues();
+    expect(component.exchangeValue.value).toEqual(2);
+    component.isBuy.next(true);
+    component.updateValues();
+    expect(component.offerRate.value).toEqual(5);
+    component.isBuy.next(false);
+    component.updateValues();
+    expect(component.offerRate.value).toEqual(4);
+  });
+
+  it('should return buysellcurr control', () => {
+    const curr = component.buySellCurr;
+    expect(curr.value).toEqual('EUR');
+  });
+
+  it('should return offercurr control', () => {
+    const curr = component.offerCurrency;
+    expect(curr.value).toEqual('PHP');
+  });
+
+  it('should return buysellamount control', () => {
+    const curr = component.buySellAmount;
+    expect(curr.value).toEqual('1.0');
   });
 });
