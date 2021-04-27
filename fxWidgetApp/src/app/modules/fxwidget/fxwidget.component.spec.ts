24e5442499da7f011092e2a9b8829c22266a9e3f
Bethsedea Joy Delos Angeles <bethsedeajoy.delosangeles@tractebel.engie.com>
2021-04-27 16:02:00 +0800

    fix failing tests- initial tests

--- fxWidgetApp/src/app/modules/fxwidget/fxwidget.component.spec.ts
+++ fxWidgetApp/src/app/modules/fxwidget/fxwidget.component.spec.ts
@@ -1,16 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormBuilder } from '@angular/forms';
+import { FxService } from 'src/app/services/fx.service';
 import { FxwidgetComponent } from './fxwidget.component';
+
 
 describe('FxwidgetComponent', () => {
   let component: FxwidgetComponent;
   let fixture: ComponentFixture<FxwidgetComponent>;
+  let fxServiceSpy: FxService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FxwidgetComponent ]
+      declarations: [ FxwidgetComponent ],
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        HttpClient,
+        FormBuilder,
+        {
+          provide: FxService, useValue: {
+            ...jasmine.createSpyObj('FxService', ['getCurrs', 'getRate', 'getBuyRate', 'getSellRate'])
+          }
+        }
+      ]
     })
     .compileComponents();
+    fxServiceSpy = TestBed.inject(FxService);
   });
 
   beforeEach(() => {
--- fxWidgetApp/src/app/services/fx.service.spec.ts
+++ fxWidgetApp/src/app/services/fx.service.spec.ts
@@ -1,12 +1,15 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { FxService } from './fx.service';
+
 
 describe('FxService', () => {
   let service: FxService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(FxService);
   });
 
